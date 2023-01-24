a49a7ea75df01035798480d6bfabcec8aa1f1a70
Cupu-Boy <fajrir357@gmail.com>
2023-01-24 13:08:21 +0700

    fix email box

--- components/hoc/withauth.js
+++ components/hoc/withauth.js
@@ -1,8 +1,10 @@
+/* eslint-disable react-hooks/rules-of-hooks */
 import React from "react";
 import { useSelector } from "react-redux";
 import { useRouter } from "next/router";
 
 const WithAuth = (Components) => {
+  // eslint-disable-next-line react/display-name
   return (props) => {
     const router = useRouter();
     const token = useSelector((state) => state.auth.token);
