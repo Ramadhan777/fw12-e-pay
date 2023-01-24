bf108f5d43664b72be3d85b9cdd5c999fdb1ba4d
Cupu-Boy <fajrir357@gmail.com>
2023-01-24 13:20:17 +0700

    add env file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env:{
-    URL_BACKEND : 'https://68xkph-8888.preview.csb.app'
-  }
 }
 
 module.exports = nextConfig
